769253456fcefe5db5c94bf0152b3e8735b57169
Leticia Amorim <66920249+leticiaamorimd@users.noreply.github.com>
2021-08-30 19:48:05 +0100

    Add files via upload

---
+++ mySketch.js
@@ -0,0 +1,36 @@
+function setup() {
+	createCanvas(900, 900, WEBGL);
+	angleMode(DEGREES)
+}
+
+function draw(){
+	background(30)
+ 
+	rotateX(60)
+	
+	noFill()
+	stroke(255)
+	
+	for(var i = 0; i <120; i++) {
+		
+		var r = map(sin(frameCount / 4), -1, -1, 100, 255)
+		var g = map(i, 0, 200, 100, 255)
+		var b = map(cos(frameCount), -1, 1, 255, 100)
+		
+		stroke(r, g, b)
+		
+		beginShape()
+	
+		for(var j = 0; j <360; j += 90) {
+		var rad = i * 8
+		var x = rad * cos(j)
+		var y = rad * sin(j)
+		var z = sin(frameCount * 2 + i * 10) * 50
+		
+		vertex(x, y, z)
+	}
+		
+	endShape(CLOSE)
+		
+	}
+}
